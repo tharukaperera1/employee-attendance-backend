2f2733952364cd2fcc0f4fd66a83d8f561f094ff
Tharuka Perera <madushantharuka13@gmail.com>
2026-05-31 00:34:57 +0530

    first commit

---
+++ src/main/java/employee_attendance/EmployeeAttendanceApplication.java
@@ -0,0 +1,13 @@
+package employee_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAttendanceApplication.class, args);
+	}
+
+}
